67f831a3b251fa310ce3a2e2993e4fb57e47d7d4
Transportnick <mazylia6@gmail.com>
2021-02-20 12:42:38 +0200

    ended test4 for adding element

--- test_list.py
+++ test_list.py
@@ -25,7 +25,7 @@ class TestListMethods(unittest.TestCase):
 
     def test_illya_melnick_fi94(self):
         lista = [2, 2, 0, 9, 2, 0, 0]
-        self.assertEqual(add_element(lista),[2, 2, 0, 9, 2, 0, 0, 1])
+        self.assertEqual(add_element(lista), [2, 2, 0, 9, 2, 0, 0, 1])
 
 
 if __name__ == '__main__':
